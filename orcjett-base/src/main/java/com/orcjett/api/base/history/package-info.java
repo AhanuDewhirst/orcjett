43d6375741048db3c1b622c19afaad772ccce561
Ahanu Dewhirst <16126069+Orcjett@users.noreply.github.com>
2021-07-04 17:41:16 +1000

    Create package-info.java

---
+++ orcjett-base/src/main/java/com/orcjett/api/base/history/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes and interfaces for performing reversible operations.
+ */
+package com.orcjett.api.base.history;
---
+++ orcjett-base/src/main/java/com/orcjett/api/base/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides classes and interfaces for performing secure application-wide operations.
+ */
+package com.orcjett.api.base.security;
